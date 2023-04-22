15908e9c8da8c6dcaec2a95e5c2c458f18b5e0b7
w0mb <danil-kovalev-04@mail.ru>
2023-04-23 04:18:26 +0700

    need git ignore for .d

---
+++ obj/src/libgeometry/geom_parser.d
@@ -0,0 +1,2 @@
+obj/src/libgeometry/geom_parser.o: src/libgeometry/geom_parser.c \
+ src/libgeometry/geom_parser.h src/libgeometry/lexer.h
---
+++ obj/src/libgeometry/lexer.d
@@ -0,0 +1,2 @@
+obj/src/libgeometry/lexer.o: src/libgeometry/lexer.c \
+ src/libgeometry/geom_parser.h src/libgeometry/lexer.h
