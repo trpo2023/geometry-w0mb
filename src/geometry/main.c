cb6f0ce2b4a160803838d5ef8de824d8755ee8d4
w0mb <danil-kovalev-04@mail.ru>
2023-04-24 03:06:39 +0700

    d

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 #include <libgeometry/geom_parser.h>
 #include <libgeometry/lexer.h>
@@ -48,16 +47,51 @@ struct circle circle_search(struct point* Center, char* arr, int* num)
 {
     struct circle Circle; // ?
 
-    double radius = radius_search(arr, num);
-
     Circle.center.x = Center->x; // ?
     Circle.center.y = Center->y; // ?
-    Circle.radius = radius;
-    Circle.perimeter = circle_perimeter(radius);
-    Circle.area = circle_area(radius);
+    Circle.radius = radius_search(arr, num);
 
     return Circle;
 };
+
+struct point2 //определение координаты по иксу игрику
+{
+    double x2;
+    double y2;
+};
+struct circle2
+{
+    struct point2 center2; // ?
+    double radius2;
+    double perimeter2;
+    double area2;
+};
+
+struct point2 center_search2(char* arr, int* num) // ищем центр круга
+{
+    struct point2 Center2; // ?
+
+    Center2.x2 = coordinat_x(arr, num);
+    Center2.y2 = coordinat_y(arr, num);
+
+    return Center2;
+};
+
+struct circle2 circle_search2(struct point2* Center2, char* arr, int* num)
+{
+    struct circle2 Circle2; // ?
+
+    double radius2 = radius_search(arr, num);
+
+    Circle2.center2.x2 = Center2->x2; // ?
+    Circle2.center2.y2 = Center2->y2; // ?
+    Circle2.radius2 = radius2;
+    Circle2.perimeter2 = circle_perimeter(radius2);
+    Circle2.area2 = circle_area(radius2);
+
+    return Circle2;
+};
+
 
 void show_circle(struct circle* Circle)
 {
@@ -70,12 +104,43 @@ void show_circle(struct circle* Circle)
 };
 
 
+void show_circle2(struct circle2* Circle2)
+{
+    printf("\ncircle2(%.2f %.2f, %.2f, %.2f, %.2f)\n",
+           Circle2->center2.x2,
+           Circle2->center2.y2,
+           Circle2->radius2,
+           Circle2->perimeter2,
+           Circle2->area2);
+};
+
+int intersection(struct circle* circle_1, struct circle2* circle_2)
+{
+    double x1 = circle_1->center.x;
+    double y1 = circle_1->center.y;
+    double radius1 = circle_1->radius;
+    //во второй структуре хранится первый круг
+    double x2 = circle_2->center2.x2;
+    double y2 = circle_2->center2.y2;
+    double radius2 = circle_2->radius2;
+    //(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1) > (r1+r2)*(r1+r2)
+    if (((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)) >= (radius1+radius2)*(radius1+radius2))
+    {
+        return 1;
+    }
+
+    else
+    {
+        return 0;
+    }
+}
 
 int main()
 {
     char enter[SIZE], figure[SIZE];
     int num = 0;
-
+    struct circle circle_1;
+    struct circle2 circle_2;
     printf("Enter a geometric figure (or press Enter for exit):\n");
     fgets(enter, SIZE, stdin);
 
@@ -108,8 +173,45 @@ int main()
         figure[num] = enter[i];
         num++;
     }
-    
 
+    char enter1[SIZE], figure1[SIZE];
+    num = 0;
+    printf("Enter an another geometric figure (or press Enter for exit):\n");
+    fgets(enter1, SIZE, stdin);
+
+    for (int i = 0; i < strlen(enter1); i++)
+    {
+        if (enter1[i] == '(' || enter1[i] == ' ')
+        {
+            to_lower(figure1, num);
+            if (strcmp(figure1, "circle") == 0)
+            {
+                struct point2 Center2 = center_search2(enter1, &num); // ?
+                struct circle2 Circle2 = circle_search2(&Center2, enter1, &num); // ?
+                is_end_empty(enter1, &num);
+                show_circle2(&Circle2);
+                break;
+            }
+            else
+            {
+                show_error(ERROR_NAME, 0, NULL);
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        else if (enter1[i] == ')')
+        {
+            show_error(ERROR_BRACKET, num, &enter1[i]);
+            exit(EXIT_FAILURE);
+        }
+
+        figure1[num] = enter1[i];
+        num++;
+    }
     
+    int a = intersection(&circle_1, &circle_2);
+    if(a == 1){printf("circles are intersection");}
+    else{printf("circles are unintersection");}
+
     return 0;
 }
