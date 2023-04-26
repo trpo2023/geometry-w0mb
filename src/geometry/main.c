9ee7c180ac69f69d6d00a47297d747c570b1953d
w0mb <danil-kovalev-04@mail.ru>
2023-04-27 03:11:03 +0700

    commits redaction

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -2,81 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include <math.h>
 #include <libgeometry/calculate.h>
-||||||| parent of c60446d (funcs for culc area and perimetr in main-file)
-
-=======
-#include <math.h>
-
->>>>>>> c60446d (funcs for culc area and perimetr in main-file)
-||||||| parent of 2d1f361 (test on calculate)
-
-=======
-#include <math.h>
-#include <libgeometry/calculate.h>
->>>>>>> 2d1f361 (test on calculate)
 #include <libgeometry/geom_parser.h>
 #include <libgeometry/lexer.h>
-<<<<<<< HEAD
 #include <libgeometry/intersect.h>
-
-||||||| parent of f0ab874 (sdf)
-
-=======
-#include <libgeometry/intersect.h>
->>>>>>> f0ab874 (sdf)
 
 #define SIZE 100
 
-<<<<<<< HEAD
-
-struct point //определение координаты по иксу игрику
-{
-    double x;
-    double y;
-};
-
-struct circle // определение радиуса
-{
-<<<<<<< HEAD
-    struct point Center; // ?
-    double radius;
-    double perimeter;
-    double area;
-||||||| parent of 2d1f361 (test on calculate)
-    struct point center; // ?
-    double radius;
-    double perimeter;
-    double area;
-=======
-    struct point center; // ?
-    float radius;
-    float perimeter;
-    float area;
->>>>>>> 2d1f361 (test on calculate)
-};
-
-||||||| parent of f0ab874 (sdf)
-
-struct point //определение координаты по иксу игрику
-{
-    double x;
-    double y;
-};
-
-struct circle // определение радиуса
-{
-    struct point Center; // ?
-    double radius;
-    double perimeter;
-    double area;
-};
-
-=======
->>>>>>> f0ab874 (sdf)
 struct point center_search(char* arr, int* num) // ищем центр круга
 {
     struct point Center; 
@@ -87,17 +20,19 @@ struct point center_search(char* arr, int* num) // ищем центр круг�
     return Center;
 };
 
+struct point2 center_search2(char* arr, int* num) // ищем центр круга
+{
+    struct point2 Center2; 
+
+    Center2.x2 = coordinat_x(arr, num);
+    Center2.y2 = coordinat_y(arr, num);
+
+    return Center2;
+};
+
 struct circle circle_search(struct point* Center, char* arr, int* num)
 {
-<<<<<<< HEAD
     struct circle Circle; 
-
-    float radius = radius_search(arr, num);
-||||||| parent of f0ab874 (sdf)
-    struct circle Circle; // ?
-=======
-    struct circle Circle; 
->>>>>>> f0ab874 (sdf)
 
     float radius = radius_search(arr, num);
 
@@ -110,16 +45,6 @@ struct circle circle_search(struct point* Center, char* arr, int* num)
     return Circle;
 };
 
-struct point2 center_search2(char* arr, int* num) // ищем центр круга
-{
-    struct point2 Center2; 
-
-    Center2.x2 = coordinat_x(arr, num);
-    Center2.y2 = coordinat_y(arr, num);
-
-    return Center2;
-};
-
  struct circle2 circle_search2(struct point2* Center2, char* arr, int* num)
  {
      struct circle2 Circle2; 
@@ -135,40 +60,8 @@ struct point2 center_search2(char* arr, int* num) // ищем центр кру�
      return Circle2;
  };
 
-<<<<<<< HEAD
-
-struct point2 center_search2(char* arr, int* num) // ищем центр круга
-{
-    struct point2 Center2; 
-
-    Center2.x2 = coordinat_x(arr, num);
-    Center2.y2 = coordinat_y(arr, num);
-
-    return Center2;
-};
-
- struct circle2 circle_search2(struct point2* Center2, char* arr, int* num)
- {
-     struct circle2 Circle2; 
-
-     float radius2 = radius_search(arr, num);
-
-     Circle2.Center2.x2 = Center2->x2; 
-     Circle2.Center2.y2 = Center2->y2; 
-     Circle2.radius2 = radius2;
-     Circle2.perimeter2 = circle_perimeter(radius2);
-     Circle2.area2 = circle_area(radius2);
-
-     return Circle2;
- };
-
-||||||| parent of 2d1f361 (test on calculate)
-
-=======
->>>>>>> 2d1f361 (test on calculate)
 void show_circle(struct circle* Circle)
 {
-<<<<<<< HEAD
     printf("\ncircle(%.2f %.2f, %.2f, %.2f, %.2f)\n",
            Circle->Center.x,
            Circle->Center.y,
@@ -185,119 +78,15 @@ void show_circle2(struct circle2* Circle2)
            Circle2->radius2,
            Circle2->perimeter2,
            Circle2->area2);
-||||||| parent of c60446d (funcs for culc area and perimetr in main-file)
-    printf("\ncircle(%.2f %.2f, %.2f)\n",
-           Circle->center.x,
-           Circle->center.y,
-           Circle->radius);
-=======
-    printf("\ncircle(%.2f %.2f, %.2f, %.2f, %.2f)\n",
-           Circle->Center.x,
-           Circle->Center.y,
-           Circle->radius,
-           Circle->perimeter,
-           Circle->area);
->>>>>>> c60446d (funcs for culc area and perimetr in main-file)
 };
 
-<<<<<<< HEAD
-void show_circle2(struct circle2* Circle2)
-{
-    printf("\ncircle2(%.2f %.2f, %.2f, %.2f, %.2f)\n",
-           Circle2->Center2.x2,
-           Circle2->Center2.y2,
-           Circle2->radius2,
-           Circle2->perimeter2,
-           Circle2->area2);
-||||||| parent of c60446d (funcs for culc area and perimetr in main-file)
-    printf("\ncircle(%.2f %.2f, %.2f)\n",
-           Circle->center.x,
-           Circle->center.y,
-           Circle->radius);
-=======
-    printf("\ncircle(%.2f %.2f, %.2f, %.2f, %.2f)\n",
-           Circle->center.x,
-           Circle->center.y,
-           Circle->radius,
-           Circle->perimeter,
-           Circle->area);
->>>>>>> c60446d (funcs for culc area and perimetr in main-file)
-};
-
-<<<<<<< HEAD
-int intersection(struct circle* circle_1, struct circle2* circle_2)
-{
-    double x1 = circle_1->center.x;
-    double y1 = circle_1->center.y;
-    double radius1 = circle_1->radius;
-    //во второй структуре хранится первый круг
-    double x2 = circle_2->Center2.x2;
-    double y2 = circle_2->Center2.y2;
-    double radius2 = circle_2->radius2;
-    //(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1) > (r1+r2)*(r1+r2)
-    if (((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)) > (radius1+radius2)*(radius1+radius2))
-    {
-        return 1;
-    }
-
-
-||||||| parent of f0ab874 (sdf)
-int intersection(struct circle* circle_1, struct circle2* circle_2)
-{
-    double x1 = circle_1->center.x;
-    double y1 = circle_1->center.y;
-    double radius1 = circle_1->radius;
-    //во второй структуре хранится первый круг
-    double x2 = circle_2->Center2.x2;
-    double y2 = circle_2->Center2.y2;
-    double radius2 = circle_2->radius2;
-    //(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1) > (r1+r2)*(r1+r2)
-    if (((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)) > (radius1+radius2)*(radius1+radius2))
-    {
-        return 1;
-    }
-
-
-=======
->>>>>>> f0ab874 (sdf)
 int main()
-{   
-
-
-||||||| parent of 137aacd (REGISTER FIX)
-int main()
-{
-=======
-int main()
-{   
-
-
->>>>>>> 137aacd (REGISTER FIX)
+{      
     char enter[SIZE], figure[SIZE];
     int num = 0;
-<<<<<<< HEAD
-
-    struct circle Circle;
-    struct circle2 Circle2;
 
     printf("Enter the first circle like that 'circle(x y,r)' or press enter to exit:\n");
-||||||| parent of 2d1f361 (test on calculate)
-    struct circle circle_1;
-    struct circle2 circle_2;
-    printf("Enter a geometric figure (or press Enter for exit):\n");
-=======
 
-    struct circle Circle;
-    struct circle2 Circle2;
-
-<<<<<<< HEAD
-    printf("Enter the first circle:\n");
->>>>>>> 2d1f361 (test on calculate)
-||||||| parent of 04674b7 (test for intersection is complited)
-    printf("Enter the first circle:\n");
-=======
-    printf("Enter the first circle like that 'circle(x y,r)' or press enter to exit:\n");
->>>>>>> 04674b7 (test for intersection is complited)
     fgets(enter, SIZE, stdin);
 
     for (int i = 0; i < strlen(enter); i++) 
@@ -308,40 +97,18 @@ int main()
             if (strcmp(figure, "circle") == 0) 
             {   
                 struct point Center = center_search(enter, &num);
-                Circle = circle_search(&Center, enter, &num);
+                struct Circle = circle_search(&Center, enter, &num);
                 is_end_empty(enter, &num);
                 show_circle(&Circle);
-<<<<<<< HEAD
-<<<<<<< HEAD
                 break;
             } 
-            else 
-||||||| parent of 2d1f361 (test on calculate)
-                break;
-            }
             else
-=======
-                circle_1 = circle_search(&Center, enter, &num);
-                // break;
-            }
-            else
->>>>>>> 2d1f361 (test on calculate)
-||||||| parent of f0ab874 (sdf)
-                circle_1 = circle_search(&Center, enter, &num);
-                // break;
-            }
-            else
-=======
-                break;
-            } 
-            else 
->>>>>>> f0ab874 (sdf)
             {
                 show_error(ERROR_NAME, 0, NULL);
                 exit(EXIT_FAILURE);
             }
         } 
-        else if (enter[i] == ')') 
+        elseif (enter[i] == ')') 
         {
             show_error(ERROR_BRACKET, num, &enter[i]);
             exit(EXIT_FAILURE);
@@ -350,44 +117,22 @@ int main()
         figure[num] = enter[i];
         num++;
     }
-    
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    char enter1[SIZE], figure1[SIZE];
-||||||| parent of 2d1f361 (test on calculate)
-    char enter1[SIZE], figure1[SIZE];
-=======
->>>>>>> 2d1f361 (test on calculate)
-    num = 0;
-<<<<<<< HEAD
-
-    printf("Enter the second circle:\n");
-||||||| parent of 2d1f361 (test on calculate)
-    printf("Enter an another geometric figure (or press Enter for exit):\n");
-=======
-||||||| parent of f0ab874 (sdf)
-    num = 0;
-=======
->>>>>>> f0ab874 (sdf)
+  
     char enter1[SIZE], figure1[SIZE];
     num = 0;
 
     printf("Enter the second circle:\n");
->>>>>>> 2d1f361 (test on calculate)
     fgets(enter1, SIZE, stdin);
 
     for (int i = 0; i < strlen(enter1); i++)
     {
-<<<<<<< HEAD
         if (enter1[i] =='(' || enter1[i] == ' ')
     {
             to_lower(figure1, num);
             if (strcmp(figure1, "circle") == 0)
             {
                 struct point2 Center2 = center_search2(enter1, &num);
-                Circle2 = circle_search2(&Center2, enter1, &num); 
+                struct Circle2 = circle_search(&Center2, enter1, &num); 
                 is_end_empty(enter1, &num);
                 show_circle2(&Circle2);
                 break;
@@ -406,101 +151,7 @@ int main()
 
         figure1[num] = enter1[i];
         num++;
-||||||| parent of 2d1f361 (test on calculate)
-        if (enter1[i] == '(' || enter1[i] == ' ')
-        {
-            to_lower(figure1, num);
-            if (strcmp(figure1, "circle") == 0)
-            {
-                struct point2 Center2 = center_search2(enter1, &num); // ?
-                struct circle2 Circle2 = circle_search2(&Center2, enter1, &num); // ?
-                is_end_empty(enter1, &num);
-                show_circle2(&Circle2);
-                break;
-            }
-            else
-            {
-                show_error(ERROR_NAME, 0, NULL);
-                exit(EXIT_FAILURE);
-            }
-        }
-        
-        else if (enter1[i] == ')')
-        {
-            show_error(ERROR_BRACKET, num, &enter1[i]);
-            exit(EXIT_FAILURE);
-        }
-
-        figure1[num] = enter1[i];
-        num++;
-=======
-        if (enter1[i] =='(' || enter1[i] == ' ')
-    {
-            to_lower(figure1, num);
-            if (strcmp(figure1, "circle") == 0)
-            {
-                struct point2 Center2 = center_search2(enter1, &num);
-                Circle2 = circle_search2(&Center2, enter1, &num); 
-                is_end_empty(enter1, &num);
-                show_circle2(&Circle2);
-                break;
-            }
-            else
-            {
-                show_error(ERROR_NAME, 0, NULL);
-                exit(EXIT_FAILURE);
-            }
     }
-    else if (enter1[i] == '(')
-    {
-        show_error(ERROR_BRACKET, num, &enter1[i]);
-        exit(EXIT_FAILURE);
-    }
-<<<<<<< HEAD
-    figure1[num] = enter1[i];
-    num++;
->>>>>>> 2d1f361 (test on calculate)
-||||||| parent of f0ab874 (sdf)
-    figure1[num] = enter1[i];
-    num++;
-=======
-
-        figure1[num] = enter1[i];
-        num++;
->>>>>>> f0ab874 (sdf)
-    }
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-    intersection(&Circle, &Circle2);
-
-||||||| parent of c60446d (funcs for culc area and perimetr in main-file)
-=======
-    
->>>>>>> c60446d (funcs for culc area and perimetr in main-file)
-||||||| parent of 2d1f361 (test on calculate)
-    
-    int a = intersection(&circle_1, &circle_2);
-    if(a == 1){printf("circles are intersection");}
-    else{printf("circles are unintersection");}
-
-=======
-    
-    int a = intersection(&circle_1, &circle_2);
-    if(a == 1){printf("circles are intersection");}
-    else{printf("circles are unintersection");}
-    
->>>>>>> 2d1f361 (test on calculate)
-||||||| parent of f0ab874 (sdf)
-    
-    int a = intersection(&circle_1, &circle_2);
-    if(a == 1){printf("circles are intersection");}
-    else{printf("circles are unintersection");}
-    
-=======
-
-    intersection(&Circle, &Circle2);
-
->>>>>>> f0ab874 (sdf)
+    intersection(Circle, Circle2);
     return 0;
 }
