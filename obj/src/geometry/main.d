f0ab87481f173852bc0c4b34ce16f39925fd3afa
w0mb <danil-kovalev-04@mail.ru>
2023-04-27 00:49:15 +0700

    sdf

--- obj/src/geometry/main.d
+++ obj/src/geometry/main.d
@@ -1,2 +1,3 @@
 obj/src/geometry/main.o: src/geometry/main.c src/libgeometry/calculate.h \
- src/libgeometry/geom_parser.h src/libgeometry/lexer.h
+ src/libgeometry/geom_parser.h src/libgeometry/lexer.h \
+ src/libgeometry/intersect.h
